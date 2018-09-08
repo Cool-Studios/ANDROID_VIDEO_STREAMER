9826dbb4f5f91652d567dd92d15b8879477a7f01
CoolboyF <coolboy_fire@outlook.com>
2018-09-08 02:16:11 +0200

    STREAMER

---
+++ app/src/main/java/com/example/coolb/androidstreamer/MainActivity.java
@@ -0,0 +1,137 @@
+package com.example.coolb.androidstreamer;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Base64;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class MainActivity extends AppCompatActivity implements MediaPlayer.OnPreparedListener, SurfaceHolder.Callback {
+    //Server Cred
+    final static String USERNAME = "";
+    final static String PASSWORD = "";
+    final static String RTSP_URL = "rtsp://184.72.239.149/vod/mp4:BigBuckBunny_175k.mov";
+
+    private MediaPlayer _mediaPlayer;
+    private SurfaceHolder _surfaceHolder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set up a full-screen black window.
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        Window window = getWindow();
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        window.setBackgroundDrawableResource(android.R.color.black);
+
+        setContentView(R.layout.activity_main);
+
+        // Configure the view that renders live video.
+        SurfaceView surfaceView =
+                (SurfaceView) findViewById(R.id.surfaceView);
+        _surfaceHolder = surfaceView.getHolder();
+        _surfaceHolder.addCallback(this);
+        _surfaceHolder.setFixedSize(320, 240);
+    }
+
+
+    //<--------- SurfaceHolder.Callback BEGIN------->
+
+    @Override
+    public void surfaceChanged(
+            SurfaceHolder sh, int f, int w, int h) {}
+
+    @Override
+    public void surfaceCreated(SurfaceHolder sh) {
+        _mediaPlayer = new MediaPlayer();
+        _mediaPlayer.setDisplay(_surfaceHolder);
+
+        Context context = getApplicationContext();
+        Map<String, String> headers = getRtspHeaders();
+        Uri source = Uri.parse(RTSP_URL);
+
+        try {
+            // Specify the IP camera's URL and auth headers.
+            _mediaPlayer.setDataSource(context, source, headers);
+
+            // Begin the process of setting up a video stream.
+            _mediaPlayer.setOnPreparedListener(this);
+            _mediaPlayer.prepareAsync();
+        }
+        catch (Exception e) {}
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder sh) {
+        _mediaPlayer.release();
+    }
+
+
+    //<--------------- RSTP Auth BEGIN------------->
+    private Map<String, String> getRtspHeaders() {
+        Map<String, String> headers = new HashMap<String, String>();
+        String basicAuthValue = getBasicAuthValue(USERNAME, PASSWORD);
+        headers.put("Authorization", basicAuthValue);
+        return headers;
+    }
+
+    private String getBasicAuthValue(String usr, String pwd) {
+        String credentials = usr + ":" + pwd;
+        int flags = Base64.URL_SAFE | Base64.NO_WRAP;
+        byte[] bytes = credentials.getBytes();
+        return "Basic " + Base64.encodeToString(bytes, flags);
+    }
+    //<------------ RSTP AUTH END --------------->
+
+    //<-------------  MediaPlayer.OnPreparedListener  BEGIN-------->
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        _mediaPlayer.start();
+    }
+
+
+//<----------------------   SurfaceHolder END       ------------------------>
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
